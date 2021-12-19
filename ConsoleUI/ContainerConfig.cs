ec849d78dd01f511534fd2c79df3c6335205fe77
Mike <michael.a.gould@gmail.com>
2021-12-18 19:34:15 -0500

    Programmatically adding classes in the Utilities folder and assigning them to their Interfaces using link and reflection

--- ConsoleUI/ContainerConfig.cs
+++ ConsoleUI/ContainerConfig.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
+using DemoLibrary;
 
 namespace ConsoleUI
 {
-    internal class ContainerConfig
+    public static class ContainerConfig
     {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
+                .Where(x => x.Namespace.Contains("Utilities"))
+                .As(x => x.GetInterfaces().FirstOrDefault(i => i.Name == "I"+ x.Name));
+            return builder.Build();
+        }
     }
 }
