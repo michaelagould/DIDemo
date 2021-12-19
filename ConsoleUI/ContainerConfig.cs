b1f57b557d95daed5ce4d55ee9410503bf58b0e1
Mike <michael.a.gould@gmail.com>
2021-12-18 19:40:10 -0500

    Added new Application Class to kick off the code, to prevent static keyword limitation for Main()

--- ConsoleUI/ContainerConfig.cs
+++ ConsoleUI/ContainerConfig.cs
@@ -14,6 +14,7 @@ namespace ConsoleUI
         public static IContainer Configure()
         {
             var builder = new ContainerBuilder();
+            builder.RegisterType<Application>().As<IApplication>();
             builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
             builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
                 .Where(x => x.Namespace.Contains("Utilities"))
